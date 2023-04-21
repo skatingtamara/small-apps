a46642eef086c76f7545501da10c751160f64345
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-04-21 09:58:40 -0700

    initial stopwatch app

---
+++ stopwatch-app/OOPInJSWithMosh-stopwatch.js
@@ -0,0 +1,43 @@
+// https://www.youtube.com/watch?v=PFmuCDHHpwk&ab_channel=ProgrammingwithMosh
+// Object-oriented Programming in JavaScript: Made Super Simple | Mosh
+
+function MakeStopwatch(){
+    let startTime, endTime, running, duration = 0
+
+    this.start = function(){
+        if (running) {
+            throw new Error('Stopwatch has already started')
+        }
+
+        running = true
+        startTime = new Date()
+    }
+    this.stop = function(){
+        if (!running){
+            throw new Error('Stopwatch has not started')
+        }
+        endTime = new Date()
+        running = false
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000
+        duration += seconds
+    }
+    this.reset = function(){
+        startTime = null
+        endTime = null
+        running = false
+        duration = 0
+    }
+
+    Object.defineProperty(this, 'duration', {
+        get: function() { return duration}
+    })
+
+}
+
+
+
+
+
+
+const sw = new MakeStopwatch()
