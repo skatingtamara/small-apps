606d8d806c993c86608530a8bc69d20cae5a54c6
skatingtamara <98510821+skatingtamara@users.noreply.github.com>
2023-05-01 21:03:52 -0700

    carousel with setinterval

--- drinkSearchAndCycle/js/main.js
+++ drinkSearchAndCycle/js/main.js
@@ -5,31 +5,58 @@ document.querySelector('button').addEventListener('click', getDrink)
 function getDrink(){
     let drink = document.querySelector('input').value
     
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        const delay = ms => new Promise( res => setTimeout(res, ms))
-        
-        const showDrink = async () => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
 
-            for(let cycle = 0; cycle < 3; cycle ++){
-                console.log(`Cycle #${cycle}`)
-                for(let i = 0; i < data.drinks.length; i++){
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+                let i = 0
+                setInterval(() =>{
+                    console.log(i)
                     console.log(data.drinks[i])
                     document.querySelector('h2').innerText = data.drinks[i].strDrink
                     document.querySelector('img').src = data.drinks[i].strDrinkThumb
                     document.querySelector('h3').innerText = data.drinks[i].strInstructions
+                    i++
+                    if(i >= data.drinks.length){
+                        i = 0
+                    }
+                }, 1000)
+                })
+        
+        .catch(err => {
+            console.log(`error ${err}`)
+    })
     
-                    await delay(3000)
-                }
-            }
-        } 
-        showDrink()
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
-
+    
 }
+
+
+
+// fetch(url)                                       // Solution with only a limited loop
+//     .then(res => res.json())
+//     .then(data => {
+//         const delay = ms => new Promise( res => setTimeout(res, ms))
+        
+//         const showDrink = async () => {
+
+//             for(let cycle = 0; cycle < 3; cycle ++){
+//                 console.log(`Cycle #${cycle}`)
+//                 for(let i = 0; i < data.drinks.length; i++){
+//                     console.log(data.drinks[i])
+//                     document.querySelector('h2').innerText = data.drinks[i].strDrink
+//                     document.querySelector('img').src = data.drinks[i].strDrinkThumb
+//                     document.querySelector('h3').innerText = data.drinks[i].strInstructions
+    
+//                     await delay(3000)
+//                 }
+//             }
+//         } 
+//         showDrink()
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     })
+
+// }
 
